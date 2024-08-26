dfc003d3846a63cb0185a95f92fbe74a28acad11
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-08-26 18:55:59 +0100

    Feat: Change id type to uuid

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -2,8 +2,8 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class File {
-  @PrimaryGeneratedColumn()
-  public id: number;
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
 
   @Column()
   public url: string;
