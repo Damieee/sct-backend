215c80017a28152345f1c1c5034493f0a8f4e178
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-09-23 16:30:48 +0100

    Feat: Add advanced search feature to get events dto

--- src/events/dto/get-events.dto.ts
+++ src/events/dto/get-events.dto.ts
@@ -1,9 +1,67 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import {
+  IsDateString,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { EventType } from '../event-type.enum';
+import { Type } from 'class-transformer';
+import { Category } from '../category.enum';
 
 export class filterDto {
   @ApiProperty()
   @IsString()
   @IsOptional()
-  search: string;
+  location: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty({
+    description: 'Search by Cost',
+    example: 4500,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  pricing: number;
+
+  @ApiProperty({
+    description: 'Search by datetime of the event (ISO 8601 format)',
+    example: '2020-02-05T06:35:22.000Z',
+    format: 'date-time', // This indicates ISO 8601 date-time format
+  })
+  @IsOptional()
+  @IsDateString(
+    { strict: true },
+    { message: 'Start date must be in ISO 8601 format' },
+  )
+  date: string;
+
+  @ApiProperty({
+    description: 'Type of the Event',
+    example: 'In-Person',
+    enum: EventType,
+  })
+  @IsEnum(EventType)
+  @IsOptional()
+  type: EventType;
+
+  @ApiProperty({
+    description: 'Category of the Event',
+    example: 'FinTech',
+    enum: Category,
+  })
+  @IsEnum(Category)
+  @IsOptional()
+  category: Category;
 }
