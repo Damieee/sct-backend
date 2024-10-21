fd6733580f227f3840938a22c7bade0af7bdc366
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-10-21 10:56:58 +0100

    Feat: Implement Google Oauth in Sevices

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -4,7 +4,6 @@ import { S3 } from 'aws-sdk';
 import { ConfigService } from '@nestjs/config';
 import { v4 as uuid } from 'uuid';
 import { FileRepository } from './files.repository';
-import axios from 'axios';
 
 @Injectable()
 export class FilesService {
@@ -48,25 +47,12 @@ export class FilesService {
     }
   }
 
-  async uploadGoogleFileFromUrl(url: string) {
-    const s3 = new S3();
-    const response = await axios({
-      method: 'GET',
-      url,
-    });
-    const dataBuffer = Buffer.from(response.data, 'binary');
+  async uploadGoogleUrl(url: string) {
     const filename = url.split('/').pop();
-    const uploadResult = await s3
-      .upload({
-        Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
-        Body: dataBuffer,
-        Key: `${uuid()}-${filename}`,
-      })
-      .promise();
 
     const newFile = this.publicFilesRepository.create({
-      key: uploadResult.Key,
-      url: uploadResult.Location,
+      key: filename,
+      url,
     });
     await this.publicFilesRepository.save(newFile);
     return newFile;
