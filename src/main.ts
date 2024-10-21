9dc3d4fc046faff401d153a61f4cafc2c613b9f4
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2024-10-21 10:55:19 +0100

    Feat: Add google oauth in swagger ui

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,19 @@ async function bootstrap() {
       { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
       'JWT',
     )
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        authorizationCode: {
+          authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
+          tokenUrl: 'https://oauth2.googleapis.com/token',
+          scopes: {
+            email: 'Access email',
+            profile: 'Access user profile',
+          },
+        },
+      },
+    })
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
 
@@ -33,6 +46,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api-docs', app, document, {
     swaggerOptions: {
+      oauth2RedirectUrl: 'http://localhost:3000/auth/google/callback',
       persistAuthorization: true,
     },
   });
